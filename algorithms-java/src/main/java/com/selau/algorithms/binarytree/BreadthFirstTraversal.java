9fa9de349120509a907fa872c13d567c98f0299b
Selau <guisrx@gmail.com>
2015-08-31 08:26:03 -0300

    traversing 2

--- algorithms-java/src/main/java/com/selau/algorithms/binarytree/BreadthFirstTraversal.java
+++ algorithms-java/src/main/java/com/selau/algorithms/binarytree/BreadthFirstTraversal.java
@@ -1,5 +1,28 @@
 package com.selau.algorithms.binarytree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.junit.Test;
+
+
 public class BreadthFirstTraversal {
+
+
+    public void traverse(final Node root) {
+        final Queue<Node> queue = new LinkedList<Node>();
+
+        queue.add(root);
+
+        for ()
+    }
+
+    @Test
+    public void test() {
+
+
+
+    }
+
 
 }
--- algorithms-java/src/main/java/com/selau/algorithms/binarytree/Node.java
+++ algorithms-java/src/main/java/com/selau/algorithms/binarytree/Node.java
@@ -33,5 +33,4 @@ public class Node {
         return payload;
     }
 
-
 }
